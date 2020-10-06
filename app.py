06a130577c2b73a6ba9a4636e4e9f2a8045e17f3
Daria <chestnutdasha.2003@gmail.com>
2020-10-06 22:07:19 +0300

    Finally

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
 from flask import Flask, render_template
+import data
 
+tours = data.tours  # список туров
+departures = data.departures  # направления
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html')  # Главная
+    b={}
+    for i in tours:
+        if i<=6:
+            defy = tours[i]
+            b[i] = defy
+    return render_template('index.html', tours=tours, departures=departures, b=b)  # Главная
 
 
 @app.route('/departures/<departure>/')
 def departure(departure):
-    return render_template('departure.html')  # Направления
+    b = {}
+    for i in tours:
+        if departure in tours[i].values():
+            defy = tours[i]
+            b[i] = defy
+    return render_template('departure.html', b=b, tours=tours, departures=departures, departure=departure)  #
+    # Направления
 
 
-@app.route('/tours/<id>/')
+@app.route('/tours/<int:id>/')
 def tour(id):
-    return render_template("tour.html", )  # Туры
-
+    b = {} # один тур
+    for i in tours:
+        if id == i:
+            defy = tours[i]
+            b[i] = defy
+    return render_template("tour.html", b=b, tours=tours, departures=departures, id=id)  # Тур конкретный
 
 
 app.run()
